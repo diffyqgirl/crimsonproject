5f892decc6630769c108d4c21a48b1402c65388b
Laura Ungar <ungar@college.harvard.edu>
2016-12-01 21:40:03 -0500

    oops forgot to add files

---
+++ js/lauravars.js
@@ -0,0 +1,10 @@
+var d11_body = "wow so much cool content"
+
+var d11_head = "Department 11"
+var d11_content = format_tooltip(d11_head, d11_body);
+function format_tooltip(header, body)
+{
+    return "<b>" + header + "</b> <br />" + body;
+}
+
+var moriban_content = "<b>Morbihan</b> <br /> Bretagne";
